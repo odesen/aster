a00b764080577044227fd710c2865c25aaa570ba
odesen <odesen@users.noreply.github.com>
2023-02-12 18:59:38 +0100

    refactor: reorganize api app

--- src/aster/api.py
+++ src/aster/api.py
@@ -6,22 +6,12 @@ from aster.posts.api import posts_router
 
 
 def create_app() -> fastapi.FastAPI:
-    from aster.database import engine
-    from aster.models import Base
-
     app = fastapi.FastAPI()
 
     async def healthcheck() -> fastapi.Response:
         return fastapi.Response("Hello world!")
 
     app.add_api_route("/health", healthcheck)
-
-    async def startup() -> None:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
-
-    app.add_event_handler("startup", startup)
 
     app.include_router(user_router)
     app.include_router(users_router)
