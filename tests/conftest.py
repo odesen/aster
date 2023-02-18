3fbbc4dd80d542e56d221530fbc7cbc6ea4baf12
odesen <odesen@users.noreply.github.com>
2023-02-18 22:48:00 +0100

    test: add api routes in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import os
-from typing import Any, Iterable
+from typing import Any, Callable, Iterable
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import Response
 
 from aster.api import create_app
+from aster.auth.schemas import UserCreate
 from aster.database import drop_database, engine, init_database
+from aster.posts.schemas import PostCreate
 
 pytestmark = pytest.mark.anyio
 
@@ -22,3 +25,135 @@ async def database() -> Any:
 @pytest.fixture(scope="session")
 def client() -> Iterable[TestClient]:
     yield TestClient(create_app())
+
+
+# Auth API
+
+
+@pytest.fixture
+def api_login(client: TestClient) -> Callable[[Any], Response]:
+    def _r(data_in: Any) -> Response:
+        return client.post(f"{client.base_url}/login", data=data_in)
+
+    return _r
+
+
+@pytest.fixture
+def api_register_user(client: TestClient) -> Callable[[UserCreate], Response]:
+    def _r(data_in: UserCreate) -> Response:
+        return client.post(f"{client.base_url}/users", content=data_in.json())
+
+    return _r
+
+
+@pytest.fixture
+def api_list_users(client: TestClient) -> Callable[[], Response]:
+    def _r() -> Response:
+        return client.get(f"{client.base_url}/users")
+
+    return _r
+
+
+@pytest.fixture
+def api_get_user(client: TestClient) -> Callable[[str], Response]:
+    def _r(username: str) -> Response:
+        return client.get(f"/users/{username}")
+
+    return _r
+
+
+@pytest.fixture
+def api_get_authenticated(client: TestClient) -> Callable[[], Response]:
+    def _r() -> Response:
+        return client.get(f"{client.base_url}/user")
+
+    return _r
+
+
+@pytest.fixture
+def api_update_authenticated_user(client: TestClient) -> Callable[[], Response]:
+    def _r() -> Response:
+        return client.patch(f"{client.base_url}/user")
+
+    return _r
+
+
+@pytest.fixture
+def api_update_password_authenticated_user(
+    client: TestClient,
+) -> Callable[[], Response]:
+    def _r() -> Response:
+        return client.put(f"{client.base_url}/user/password")
+
+    return _r
+
+
+@pytest.fixture
+def api_list_users_blocked_by_authenticated_user(
+    client: TestClient,
+) -> Callable[[], Response]:
+    def _r() -> Response:
+        return client.get(f"{client.base_url}/user/blocks")
+
+    return _r
+
+
+@pytest.fixture
+def api_check_user_blocked_by_authenticated_user(
+    client: TestClient,
+) -> Callable[[str], Response]:
+    def _r(username: str) -> Response:
+        return client.get(f"{client.base_url}/user/blocks/{username}")
+
+    return _r
+
+
+@pytest.fixture
+def api_block_user(client: TestClient) -> Callable[[str], Response]:
+    def _r(username: str) -> Response:
+        return client.put(f"{client.base_url}/user/blocks/{username}")
+
+    return _r
+
+
+@pytest.fixture
+def api_unblock_user(client: TestClient) -> Callable[[str], Response]:
+    def _r(username: str) -> Response:
+        return client.delete(f"{client.base_url}/user/blocks/{username}")
+
+    return _r
+
+
+# Post API
+
+
+@pytest.fixture
+def api_create_post(client: TestClient) -> Callable[[PostCreate], Response]:
+    def _r(data_in: PostCreate) -> Response:
+        return client.post(f"{client.base_url}/posts", content=data_in.json())
+
+    return _r
+
+
+@pytest.fixture
+def api_list_posts(client: TestClient) -> Callable[[str], Response]:
+    def _r(username: str) -> Response:
+        return client.get(f"{client.base_url}/posts", params={"username": username})
+
+    return _r
+
+
+@pytest.fixture
+def api_get_post(client: TestClient) -> Callable[[int], Response]:
+    def _r(post_id: int) -> Response:
+        return client.get(f"{client.base_url}/posts/{post_id}")
+
+    return _r
+
+
+@pytest.fixture
+def api_delete_post(client: TestClient) -> Callable[[int], Response]:
+    def _r(post_id: int) -> Response:
+        return client.delete(f"{client.base_url}/posts/{post_id}")
+
+    return _r
