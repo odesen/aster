7cc2944fd159d6026eef95f40a367e5f13821f88
odesen <odesen@users.noreply.github.com>
2023-03-06 21:56:50 +0100

    feat: open telemetry exporter jaeger

--- src/aster/api.py
+++ src/aster/api.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, Response
+
+# from opentelemetry import trace
+# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
+# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aster.auth.api import login_router, user_router, users_router
 from aster.logging import StructLoggingConfig
 from aster.middlewares import (
@@ -32,4 +38,10 @@ def create_app() -> FastAPI:
     app.include_router(posts_router)
 
     FastAPIInstrumentor.instrument_app(app)
+    # resource = Resource(attributes={SERVICE_NAME: "aster"})
+    # jaeger_exporter = JaegerExporter(agent_host_name="localhost", agent_port=6831)
+    # provider = TracerProvider(resource=resource)
+    # processor = BatchSpanProcessor(jaeger_exporter)
+    # provider.add_span_processor(processor)
+    # trace.set_tracer_provider(provider)
     return app
